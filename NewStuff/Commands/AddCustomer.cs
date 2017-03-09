a4717600d0845daf9c24441f61171c59ed0d0617
Alexey Zimarev <alzi@abax.no>
2017-03-09 11:48:11 +0100

    Handling with pattern matching

---
+++ NewStuff/Commands/AddCustomer.cs
@@ -0,0 +1,9 @@
+﻿namespace NewStuff.Commands
+{
+    public class AddCustomer
+    {
+        public string Name { get; set; }
+        public string Street1 { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ NewStuff/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NewStuff.Commands;
+using NewStuff.Events;
+
+namespace NewStuff
+{
+    public class Customer
+    {
+        public Guid Id { get; }
+        private string _name;
+
+        private Customer(Guid id)
+        {
+            Id = id;
+        }
+
+        public void Handle(object command)
+        {
+            switch (command)
+            {
+                case AddCustomer add:
+                    When(new CustomerAdded {Name = add.Name, City = add.City, Street1 = add.Street1});
+                    break;
+            }
+        }
+
+        public void Apply(object @event)
+        {
+            // add to uncommitted changes and
+            When(@event);
+        }
+
+        public void When(object @event)
+        {
+            switch (@event)
+            {
+                case CustomerAdded added:
+                    _name = added.Name;
+                    break;
+            }
+        }
+    }
+}
